8682cafb2d5fcbdcd36ea9d4402b571003fcf358
Zinthia <zinthiadesign@gmail.com>
2021-11-30 15:26:50 -0500

    Angula Ejercicios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,40 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
 })
 export class AppComponent {
-  title = 'bases';
+  // titulo: string = 'Contador App';
+  // numero: number = 10;
+  // base: number = 5;
+  // // base2: number = 5;
+  // acumular( valor: number ): void {
+  //   this.numero += valor;
+  // }
+
 }
+
+
+
+
+  // suma( base2: number ): void {
+  //   this.base += base2;
+  // }
+  // resta(base2: number): void{
+  //   this.base -= base2;
+  // }
+
+  // acumular( valor: number ): void {
+  //   this.numero += valor;
+  // }
+
+
+
+  // sumar(num1: number, num2: number): number {
+  //   return num1 + num2;
+  // }
+
+  // letra(palabra: string): string {
+  //   return palabra;
+  // }
+
